2345db5dc2fbd3b3f14263c7813cbc97d6f3aff5
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-01-19 23:25:41 +0530

    Refactor PersonalDetailsGrid component and update PersonalDetails page

--- homly/src/Components/PersonalDetailsGrid/PersonalDetailsGrid.js
+++ homly/src/Components/PersonalDetailsGrid/PersonalDetailsGrid.js
@@ -1,39 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-import {Grid,Typography,Button} from '@mui/material'
+import { Grid, Typography, Button } from "@mui/material";
 
 const PersonalDetailsGrid = (props) => {
-    return(
-        <Grid container>
-                    <Grid item md={4}>
-                      <Typography
-                        variant="h6"
-                        fontWeight={"regular"}
-                        component="div"
-                      >
-                        {props.lable}
-                      </Typography>
-                    </Grid>
-                    <Grid item md={6}>
-                      <Typography
-                        variant="h6"
-                        fontWeight={"regular"}
-                        component="div"
-                      >
-                        {props.value}
-                      </Typography>
-                    </Grid>
-                    <Grid item md={2}>
-                      <Button
-                        size="small"
-                        variant="outline"
-                        style={{ display: props.editable ? "block" : "none" }}
-                      >
-                        Edit
-                      </Button>
-                    </Grid>
-                  </Grid>
-    )
-}
+    const [buttonName, setButtonName] = useState("Edit");
 
-export default PersonalDetailsGrid
+    const handleEdit = () => {
+        if (buttonName === "Edit") {
+            setButtonName("Save");
+        } else {
+            setButtonName("Edit");
+        }
+    }
+    return (
+        <Grid container key={props.id}>
+            <Grid item md={4}>
+                <Typography variant="h6" fontWeight={"regular"} component="div">
+                    {props.lable}
+                </Typography>
+            </Grid>
+            <Grid item md={6}>
+                <Typography variant="h6" fontWeight={"regular"} component="div">
+                    {props.value}
+                </Typography>
+            </Grid>
+            <Grid item md={2}>
+                <Button
+                    variant="contained"
+                    size="small"
+                    sx={{ backgroundColor:"primary.main" }}
+                    style={{ display: props.editable ? "block" : "none" }}
+                    onClick={handleEdit}
+                >
+                    {buttonName}
+                </Button>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default PersonalDetailsGrid;
--- homly/src/Pages/PersonalDetails/PersonalDetails.js
+++ homly/src/Pages/PersonalDetails/PersonalDetails.js
@@ -18,8 +18,6 @@ import {
 
 import theme from "../../HomlyTheme";
 
-
-
 const  userServiceNo = '214002';
 
 const PersonalDetails = () => {
@@ -38,8 +36,8 @@ const PersonalDetails = () => {
   const userd = detailsE.filter(obj => {
     return obj.serviceNumber === userServiceNo;
   });
-
-  console.log(userd[0] && userd[0].name);
+  // let arr =Object.keys(userd[0]) ;
+  // {arr[0] && console.log(arr[0])};
 
   // let len = userd === null ? 0 : userd.length;
 
@@ -72,13 +70,13 @@ const PersonalDetails = () => {
                 </Box> */}
             <Card sx={{ width: "90%", height: "90%" }}>
               <CardContent sx={{ display: "flex", flexDirection: "column" }}>
-                {userd[0] && <PersonalDetailsGrid lable="Service Number" value={userd[0].serviceNumber} editable={false} />}
-                {userd[0] && <PersonalDetailsGrid lable="Name" value={userd[0].name} editable={false} />}
-                {userd[0] && <PersonalDetailsGrid lable="Contact Number" value={userd[0].contactNo} editable={true} />}
-                {userd[0] && <PersonalDetailsGrid lable="Email" value={userd[0].email} editable={true} />}
-                {userd[0] && <PersonalDetailsGrid lable="NIC Number" value={userd[0].NIC} editable={false} />}
-                {userd[0] && <PersonalDetailsGrid lable="Work Location" value={userd[0].workLocation} editable={false} />}
-                {userd[0] && <PersonalDetailsGrid lable="Residantal Address" value={userd[0].address} editable={true} />}
+                {userd[0] && <PersonalDetailsGrid id="serviceNo" lable="Service Number" value={userd[0].serviceNumber} editable={false} />}
+                {userd[0] && <PersonalDetailsGrid id="name" lable="Name" value={userd[0].name} editable={false} />}
+                {userd[0] && <PersonalDetailsGrid id="contactNo" lable="Contact Number" value={userd[0].contactNo} editable={true} />}
+                {userd[0] && <PersonalDetailsGrid id="email" lable="Email" value={userd[0].email} editable={true} />}
+                {userd[0] && <PersonalDetailsGrid id="nic" lable="NIC Number" value={userd[0].NIC} editable={false} />}
+                {userd[0] && <PersonalDetailsGrid id="work" lable="Work Location" value={userd[0].workLocation} editable={false} />}
+                {userd[0] && <PersonalDetailsGrid id="address" lable="Residantal Address" value={userd[0].address} editable={true} />}
 
                 
               </CardContent>
