bce7c216f11f8d343591e6703e718a9e415db7b5
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-01-21 01:21:27 +0530

    Add handlePersonalDetails function to Editable component

--- homly/src/Components/PersonalDetailsGrid/Editable.js
+++ homly/src/Components/PersonalDetailsGrid/Editable.js
@@ -6,14 +6,17 @@ import { Grid, Typography, Button, TextField } from "@mui/material";
 import { EditPersonalDetailsContext } from "../../Contexts/EditPersonalDetailsContext";
 
 const Editable = (props) => {
-  const { handleEditId} = useContext(EditPersonalDetailsContext);
+  const { handleEditId,handlePersonalDetails} = useContext(EditPersonalDetailsContext);
   const [data, setData] = useState(props.value);
 
   const handleSave = () => {
     {
       props.id && handleEditId("");
     }
-  
+    {
+      props.id && handlePersonalDetails([props.id,data]);
+    }
+    console.log(data);
     console.log("save");
   };
 
--- homly/src/Contexts/EditPersonalDetailsContext.js
+++ homly/src/Contexts/EditPersonalDetailsContext.js
@@ -5,17 +5,23 @@ export const EditPersonalDetailsContext = createContext();
 
 const EditPersonalDetailsContextProvider = ({ children }) => {
   const [currentEditId, setCurrentEditId] = useState(null);
-//   const [userPersonalDetails, setUserPersonalDetails] = useState({});
+  const [userPersonalDetails, setUserPersonalDetails] = useState({});
 
   const handleEditId = (id) => {
     setCurrentEditId(id);
     console.log(id);
   };
 
-  
+const handlePersonalDetails = (data) => {
+    setUserPersonalDetails({ ...userPersonalDetails, [data[0]]: data[1] });
+    console.log(userPersonalDetails);
+
+    // console.log("keu",Object.keys(object),'val',Object.values(object));
+};
+
   return (
     <EditPersonalDetailsContext.Provider
-      value={{ currentEditId,  handleEditId }}
+      value={{ currentEditId, handleEditId, handlePersonalDetails }}
     >
       {children}
     </EditPersonalDetailsContext.Provider>
