685ee60504a6e946320604854de5b78b32532632
Kavindu Lakshan Munasinghe <104860051+LakshanKavindu@users.noreply.github.com>
2024-01-21 15:49:52 +0530

    Refactor code for improved performance

---
+++ homly/src/Pages/LocationAdmin/CreateHolidayHome.jsx
@@ -0,0 +1,41 @@
+
+import '../App.css';
+import React,{ useState} from 'react'
+
+import Box from '@mui/material/Box';
+import { Grid,ThemeProvider,Container,Button,Typography} from '@mui/material';
+import theme from '../Homlytheme';
+
+import SideNavbar from '../components/SideNavbar'
+import DashboardContent from '../components/DashboardContent';
+import PageTitle from '../components/PageTitle';
+import ControlPointIcon from '@mui/icons-material/ControlPoint';    
+import CreateHolidayHomeContent from '../components/CreateHolidayHome/CreateHolidayHomeContent';
+
+
+const Report = () => {
+
+    const [showNav,setShowNav] = useState('nav_grid_deactive')
+
+    return (
+    <ThemeProvider theme={theme}>
+        <Box className="main_container" sx={{width:"100%",backgroundColor:'primary.main',overflow:'hidden'}}>
+                <Container maxWidth="xl" style={{padding:0}}>
+                    <Grid container sx={{position:'relative'}}>
+                        <Grid className={showNav} xs={3} sx={{backgroundColor:"primary.main",height:"100vh" }}>
+                            <SideNavbar setShowNav={setShowNav}></SideNavbar>
+                        </Grid>
+                        <Grid className='container_grid' xs={9} sx={{backgroundColor:'white',borderTopLeftRadius:'20px',padding:'10px 30px',height:'100vh',position:'relative'}}>
+                            <Box sx={{height:"100%",paddingBottom:'9em'}}>
+                                <PageTitle setShowNav={setShowNav} title={'Create Holiday Home'} bell={true}/>
+                                <CreateHolidayHomeContent/>
+                            </Box>
+                        </Grid>  
+                    </Grid>
+                </Container>    
+            </Box>
+    </ThemeProvider>
+  )
+}
+
+export default Report
---
+++ homly/src/Pages/LocationAdmin/HolidayHomeDetails.jsx
@@ -0,0 +1,42 @@
+
+import '../App.css';
+import React,{ useState} from 'react'
+
+import Box from '@mui/material/Box';
+import { Grid,ThemeProvider,Container,Button,Typography} from '@mui/material';
+import theme from '../Homlytheme';
+
+import SideNavbar from '../components/SideNavbar'
+import DashboardContent from '../components/DashboardContent';
+import PageTitle from '../components/PageTitle';
+import ControlPointIcon from '@mui/icons-material/ControlPoint';    
+
+
+const Report = () => {
+
+    const [showNav,setShowNav] = useState('nav_grid_deactive')
+
+    return (
+    <ThemeProvider theme={theme}>
+        <Box className="main_container" sx={{width:"100%",backgroundColor:'primary.main',overflow:'hidden'}}>
+                <Container maxWidth="xl" style={{padding:0}}>
+                    <Grid container sx={{position:'relative'}}>
+                        <Grid className={showNav} xs={3} sx={{backgroundColor:"primary.main",height:"100vh" }}>
+                            <SideNavbar setShowNav={setShowNav}></SideNavbar>
+                        </Grid>
+                        <Grid className='container_grid' xs={9} sx={{backgroundColor:'white',borderTopLeftRadius:'20px',padding:'10px 30px',height:'100vh',position:'relative'}}>
+                            <Box sx={{height:"100%"}}>
+                                <PageTitle setShowNav={setShowNav} title={'Holiday Homes Details'} bell={true}/>
+                               
+                                
+                                            
+                            </Box>
+                        </Grid>  
+                    </Grid>
+                </Container>    
+            </Box>
+    </ThemeProvider>
+  )
+}
+
+export default Report
