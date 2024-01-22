b376b2cd1649d6100086e8ba13db0e72e3224624
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2024-01-22 22:41:46 +0530

    new button added and add new feature to view past 2 complaint (max2) addd to viewcompliant popup

--- homly/src/Components/PrimaryAdmin/ViewPopupComplints.jsx
+++ homly/src/Components/PrimaryAdmin/ViewPopupComplints.jsx
@@ -3,139 +3,208 @@ import CloseIcon from '@mui/icons-material/Close';
 import { useState } from "react";
 import { grey } from "@mui/material/colors";
 import theme from '../../HomlyTheme'
+import '../../Components/PrimaryAdmin/Css/complaintpopup.css'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+
+const ViewPopupComplaints = (props) => {
+    const [reson, setReson] = useState("")
+    const [expand, setExpand] = useState(false)
 
 
 
-const ViewPopupComplaints=(props)=>{
-    const [reson, setReson]=useState("")
-  
+    const data = [
+        { Service_number: 1, Nic_number: 27, User_name: 'Lonnie Antonioni', date: '1/31/2023', description: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available' },
+        { Service_number: 2, Nic_number: 1014, User_name: 'Carlita Cominello', date: '9/13/2023', description: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available' },
 
-    
-    return(
+    ];
+
+
+
+    return (
         <ThemeProvider theme={theme}>
-            <Box  className="popup" sx={
+            <Box className="popup" sx={
                 {
-                    position:"fixed",
-                    left:'0',
-                    right:'0',
-                    width:"100%",
-                    height:"100%",
-                    display:"flex",
-                    alignItems:"center",
-                    justifyContent:'center',
-                    backgroundColor:"rgba(0,0,0,0.5)",
-                    zIndex:100
-            
+                    position: "fixed",
+                    left: '0',
+                    right: '0',
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: 'center',
+                    backgroundColor: "rgba(0,0,0,0.5)",
+                    zIndex: 100
+
                 }
             }
-            onClick={(e) => {
-                console.log(e.target.className)
-                if (e.target.className === "popup MuiBox-root css-4e0c2m") {
-                    props.handlepopup()
-                }
-            
-              }}
-            
+                onClick={(e) => {
+                    console.log(e.target.className)
+                    if (e.target.className === "popup MuiBox-root css-4e0c2m") {
+                        props.handlepopup()
+                    }
+
+                }}
+
             >
                 <Box>
                     <Box sx={{
-                        backgroundColor:'white',
-                        borderRadius:"10px",
-                       padding:'20px',
-                       width:{md:'400px'},
-                        display:'flex',
-                        flexDirection:'column',
-                        justifyContent:"center",
-                        alignItems:'center',
-                        position:'relative',
-            
+                        backgroundColor: 'white',
+                        borderRadius: "10px",
+                        padding: '5px',
+                        width: { md: '470px' },
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: "center",
+                        alignItems: 'center',
+                        position: 'relative',
+
                     }}>
-                       <Button sx={{position:"absolute",right:"0px",top:'0px',padding:"10px",borderRadius:'50%',color:'black'}} onClick={()=>{props.handlepopup()}}>X</Button>
-            
-                    <Box sx={{
-                        display:'felx',
-                        flexDirection:'column',
-                        // width:'100%',
-            
-            
-                    }}>
-                        <Box sx={{
-                            // width:"100%",
-                            display:'flex',
-                            flexDirection:{md:'row',xs:'column'},
-                            justifyContent:"space-between",
-                            alignItems:'center',
-                            margin:'10px'
-                        }}>
-                            <Box><Typography sx={{flex:2}} h6>Service Number</Typography></Box>
-                            <Box ><TextField
-                            
-                            disabled value={props.selecteduser.Service_number} size="small" sx={{width:'85%',margin:"5px"}}></TextField></Box>
-                        </Box>
+                        <Button sx={{ position: "absolute", right: "0px", top: '0px', padding: "10px", borderRadius: '50%', color: 'black' }} onClick={() => { props.handlepopup() }}>X</Button>
 
                         <Box sx={{
-                            // width:"100%",
-                            display:'flex',
-                            flexDirection:{md:'row',xs:'column'},
-                            justifyContent:"space-between",
-                            alignItems:'center',
-                            margin:'10px'
-                        }}>
-                            <Box><Typography h6>Reservation Number</Typography></Box>
-                            <Box  ><TextField disabled value={props.selecteduser.Nic_number}  size="small" sx={{width:'85%',margin:"5px"}}></TextField></Box>
-                        </Box>
-
-                        <Box sx={{
-                            // width:"100%",
-                            display:'flex',
-                            flexDirection:{md:'row',xs:'column'},
-                            justifyContent:"space-between",
-                            alignItems:'center',
-                            margin:'10px'
-                        }}>
-                            <Box><Typography  h6>Date</Typography></Box>
-                            <Box ><TextField value={props.selecteduser.date} disabled size="small" sx={{width:'85%',margin:"5px"}}></TextField></Box>
-                        </Box>
+                            display: 'felx',
+                            flexDirection: 'column',
+                            // width:'100%',
 
 
-                        
-                        <Box sx={{
-                            // width:"100%",
-                            display:'flex',
-                            flexDirection:{md:'row',xs:'column'},
-                            justifyContent:"space-between",
-                            alignItems:'center',
-                            margin:'10px'
                         }}>
-                            <Box><Typography  h6>Blacklist Reson</Typography></Box>
-                            <Box ><TextField value={reson}  size="small" sx={{width:'85%',margin:"5px",} }  onChange={(e)=>{setReson(e.target.value)}}></TextField></Box>
-                        </Box>
+                            <Box sx={{
+                                // width:"100%",
+                                display: 'flex',
+                                flexDirection: { md: 'row', xs: 'column' },
+                                justifyContent: "space-between",
+                                alignItems: 'center',
+                                margin: '10px'
+                            }}>
+                                <Box><Typography sx={{ flex: 2 }} h6>Service Number</Typography></Box>
+                                <Box ><TextField
 
-                        <Box sx={{
-                            // width:"100%",
-                            display:'flex',
-                            flexDirection:{md:'row',},
-                            justifyContent:"flex-end",
-                            alignItems:'center',
-                            margin:'10px'
-                        }}>
-                            {/* <Box><Typography sx={{fontFamily:'roboto',}} h6>Service Number</Typography></Box>
+                                    disabled value={props.selecteduser.Service_number} size="small" sx={{ width: '85%', margin: "5px" }}></TextField></Box>
+                            </Box>
+
+                            <Box sx={{
+                                // width:"100%",
+                                display: 'flex',
+                                flexDirection: { md: 'row', xs: 'column' },
+                                justifyContent: "space-between",
+                                alignItems: 'center',
+                                margin: '10px'
+                            }}>
+                                <Box><Typography h6>Reservation Number</Typography></Box>
+                                <Box  ><TextField disabled value={props.selecteduser.Nic_number} size="small" sx={{ width: '85%', margin: "5px" }}></TextField></Box>
+                            </Box>
+
+                            <Box sx={{
+                                // width:"100%",
+                                display: 'flex',
+                                flexDirection: { md: 'row', xs: 'column' },
+                                justifyContent: "space-between",
+                                alignItems: 'center',
+                                margin: '10px'
+                            }}>
+                                <Box><Typography h6>Date</Typography></Box>
+                                <Box ><TextField value={props.selecteduser.date} disabled size="small" sx={{ width: '85%', margin: "5px" }}></TextField></Box>
+                            </Box>
+                            <Box sx={{
+                                // width:"100%",
+                                display: 'flex',
+                                flexDirection: { md: 'row', xs: 'column' },
+                                justifyContent: "space-between",
+                                alignItems: 'center',
+                                margin: '10px'
+                            }}>
+                                <Box><Typography h6>Description</Typography></Box>
+                                <Box  ><TextField multiline maxRows={4} disabled value={props.selecteduser.Nic_number} size="small" sx={{ width: '85%', margin: "5px", }}></TextField></Box>
+                            </Box>
+
+
+
+                            <Box sx={{
+                                // width:"100%",
+                                display: 'flex',
+                                flexDirection: { md: 'row', xs: 'column' },
+                                justifyContent: "space-between",
+                                alignItems: 'center',
+                                margin: '10px'
+                            }}>
+                                <Box><Typography h6>Blacklist Reson</Typography></Box>
+                                <Box ><TextField value={reson} size="small" sx={{ width: '85%', margin: "5px", }} onChange={(e) => { setReson(e.target.value) }}></TextField></Box>
+                            </Box>
+
+                            <Box sx={{
+                                // width:"100%",
+                                display: 'flex',
+                                flexDirection: { md: 'row', xs: 'column' },
+                                justifyContent: "space-between",
+                                alignItems: 'center',
+                                margin: '10px'
+                            }}>
+                                <div>
+                                    {data.length >= 1 ? <Accordion expanded={!expand} onClick={() => { setExpand(!expand) }}>
+                                        <AccordionSummary
+                                            expandIcon={<ArrowDropDownIcon />}
+                                            aria-controls="panel2-content"
+                                            id="panel2-header"
+                                        >
+                                            <Typography>Complaint on {data[0].date}</Typography>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <Typography>
+                                                {data[0].description}
+                                            </Typography>
+                                        </AccordionDetails>
+                                    </Accordion> : ""}
+                                    {data.length == 2 ? <Accordion expanded={expand} onClick={() => { setExpand(!expand) }}>
+                                        <AccordionSummary
+                                            expandIcon={<ArrowDropDownIcon />}
+                                            aria-controls="panel2-content"
+                                            id="panel2-header"
+                                        >
+                                            <Typography>Complaint on {data[1].date}</Typography>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <Typography>
+                                                {data[1].description}
+                                            </Typography>
+                                        </AccordionDetails>
+                                    </Accordion> : ""}
+
+                                </div>
+
+                            </Box>
+
+                            <Box sx={{
+                                // width:"100%",
+                                display: 'flex',
+                                flexDirection: { md: 'row', },
+                                justifyContent: "flex-end",
+                                alignItems: 'center',
+                                margin: '10px'
+                            }}>
+                                {/* <Box><Typography sx={{fontFamily:'roboto',}} h6>Service Number</Typography></Box>
                             <Box ></Box> */}
-                                    <Button variant='contained' sx={{marginRight:'3%'}}  >Add To Blacklist</Button>
-                                    <Button variant='outlined' sx={{marginRight:'5%'}} onClick={()=>{props.handlepopup()}} >Close</Button>
+                                <Button variant='contained' sx={{ marginRight: '3%' }}  >Send Warning</Button>
+
+                                <Button variant='contained' sx={{ marginRight: '3%' }}  >Add To Blacklist</Button>
+                                <Button variant='outlined' sx={{ marginRight: '5%' }} onClick={() => { props.handlepopup() }} >Mark</Button>
+
+                            </Box>
+
+
+
+
 
                         </Box>
-            
-            
-            
-            
-            
-                    </Box>
-            
+
                     </Box>
                 </Box>
             </Box>
-        </ThemeProvider> 
+        </ThemeProvider>
     )
 
 }
