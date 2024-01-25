f78ac2fd99adc173b60d3bd808cbe76f01bcdc54
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-01-26 00:21:57 +0530

    changes Home.js file,add top image and some details

--- homly/src/Pages/User/Home.js
+++ homly/src/Pages/User/Home.js
@@ -1,41 +1,130 @@
 import React from "react";
-import { Container, Box } from "@mui/material";
+import {
+  Container,
+  Box,
+  Typography,
+  ThemeProvider,
+  Stack, Grid,
+} from "@mui/material";
 
 import NavBar from "../../Components/NavBar/NavBar";
-import MyReservationCard from "../../Components/MyReservationCard/MyReservationCard";
+import theme from "../../HomlyTheme";
+// import MyReservationCard from "../../Components/MyReservationCard/MyReservationCard";
 
 export default function Home() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Box
-        className="main_container"
+        className="main-container-homepage"
         sx={{
           width: "100%",
           overflow: "hidden",
         }}
       >
-        <Container maxWidth="xl" style={{ padding: 0 ,height:'100vh'}}>
+        <Container maxWidth="xl" style={{ padding: 0 }}>
           <NavBar />
-          <Container sx={{ marginTop: "100px" }}>
-            <div>This is Home page.</div>
-            <MyReservationCard
-              image="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG9saWRheSUyMGhvbWV8ZW58MHx8MHx8fDA%3D"
-              title="Anuradhapura 1"
-              rating={4.5}
-              address="No 1, Anuradhapura"
-              cheking="2021-10-10"
-              checkout="2021-10-20"
-              adultCount={2}
-              childCount={3}
-              roomCount={20}
-              hallCount={1}
-              reserved="2021-09-20"
-              reservedExpire="2021-09-25"
-              paid="false"
-            />
+          <Container maxWidth="lg" sx={{ bgcolor: "chartreuse" }}>
+            <Container
+              sx={{
+                bgcolor: "azure",
+                height: "100%",
+                width: { xs: "100%", sm: "95%", padding: 0 },
+              }}
+            >
+              <Box sx={{ position: "relative" }}>
+                <Box
+                  component="img"
+                  src="https://images.pexels.com/photos/2403209/pexels-photo-2403209.jpeg"
+                  alt="top-image"
+                  sx={{ width: "100%", height: "50vh", objectFit: "cover" }}
+                />
+                <Stack
+                  direction="column"
+                  sx={{
+                    position: "absolute",
+                    height: "50vh",
+                    width: "100%",
+                    bgcolor: "#ffa07d5c",
+                    top: 0,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Stack
+                    direction="column"
+                    sx={{ color: "white", fontWeight: 700 }}
+                  >
+                    <Typography
+                      sx={{
+                        fontWeight: "bold",
+                        fontSize: { xs: "1.7rem", sm: "3rem" },
+                      }}
+                    >
+                      Discover the perfect getaway
+                    </Typography>
+                    <Typography
+                      sx={{
+                        fontWeight: "bold",
+                        fontSize: { xs: "1.7rem", sm: "3rem" },
+                      }}
+                    >
+                      with beautiful views
+                    </Typography>
+                  </Stack>
+                  <Stack
+                    direction="column"
+                    sx={{ bgcolor: "white", width: "80%" }}
+                  >
+                    <Typography>Find</Typography>
+                    <Grid container sx={{bgcolor:'blueviolet',width:'100%'}}>
+                      <Grid item md={4}>
+                        <Stack direction='column'>
+                          <Typography>Location</Typography>
+                          <Typography>Location</Typography>
+                        </Stack>
+                      </Grid>
+                      <Grid item md={6}>
+                        <Stack direction='row'>
+                          <Stack direction='column'>
+                            <Typography>Location</Typography>
+                            <Typography>Location</Typography>
+                          </Stack>
+                          <Stack direction='column'>
+                            <Typography>Location</Typography>
+                            <Typography>Location</Typography>
+                          </Stack>
+                        </Stack>
+                      </Grid>
+                      <Grid item md={2}>
+                        <Typography>Search</Typography>
+                      </Grid>
+                    </Grid>
+                  </Stack>
+                </Stack>
+                <Box></Box>
+                <Typography variant="h4" color="initial">
+                  Top Rated Holiday Homes
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h4" color="initial">
+                  our places - 5 districts
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h4" color="initial">
+                  Browse for More holiday Homes
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h4" color="initial">
+                  contactus/footer
+                </Typography>
+              </Box>
+            </Container>
           </Container>
         </Container>
       </Box>
-    </div>
+    </ThemeProvider>
   );
 }
