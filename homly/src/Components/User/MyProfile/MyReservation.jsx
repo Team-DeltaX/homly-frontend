85c16abec5e29a615ae871c6f8f473fa49097c2e
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-01-31 22:31:26 +0530

    Refactor MyReservation component to use MUI Tabs

--- homly/src/Components/User/MyProfile/MyReservation.jsx
+++ homly/src/Components/User/MyProfile/MyReservation.jsx
@@ -1,14 +1,18 @@
-// import React, { useContext } from "react";
-import React from 'react'
+import React from "react";
 import {
   Box,
-//   Container,
+  Stack,
+  //   Container,
   ThemeProvider,
   Typography,
-//   Card,
-//   CardContent,
-//   CardActions,
+  Tab,
+  Tabs,
+  Card,
+  CardContent,
+  //   CardActions,
 } from "@mui/material";
+
+
 
 // import PersonalDetailsGrid from "../PersonalDetailsGrid/PersonalDetailsGrid";
 // import UpdateButton from "../PersonalDetailsGrid/UpdateButton";
@@ -17,89 +21,45 @@ import {
 
 import theme from "../../../HomlyTheme";
 
-
 const MyReservation = () => {
-    // const { userPersonalDetails } = useContext(EditPersonalDetailsContext);
+  const [value, setValue] = React.useState("0");
+
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  
+  const tabComponent =[<div>tab1</div>,<div>tab2</div>]
+
+
   return (
     <ThemeProvider theme={theme}>
       <Box>
-          <Typography variant="h4">My Reservation</Typography>
-          {/* <Box
-            sx={{
-              width: "100%",
-              height: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              margin:{xs:'3% 0 20% 0'}
-            }}
-          >
-           
-              <Card sx={{ width: {xs:"100%",sm:"90%"}}}>
-                <CardContent sx={{ display: "flex", flexDirection: "column" }}>
-                  {userPersonalDetails && (
-                    <PersonalDetailsGrid
-                      id="serviceNo"
-                      lable="Service Number"
-                      value={userPersonalDetails.serviceNumber}
-                      editable={false}
-                    />
-                  )}
-                  {userPersonalDetails && (
-                    <PersonalDetailsGrid
-                      id="name"
-                      lable="Name"
-                      value={userPersonalDetails.name}
-                      editable={false}
-                    />
-                  )}
-                  {userPersonalDetails && (
-                    <PersonalDetailsGrid
-                      id="contactNo"
-                      lable="Contact Number"
-                      value={userPersonalDetails.contactNo}
-                      editable={true}
-                    />
-                  )}
-                  {userPersonalDetails && (
-                    <PersonalDetailsGrid
-                      id="email"
-                      lable="Email"
-                      value={userPersonalDetails.email}
-                      editable={true}
-                    />
-                  )}
-                  {userPersonalDetails && (
-                    <PersonalDetailsGrid
-                      id="nic"
-                      lable="NIC Number"
-                      value={userPersonalDetails.NIC}
-                      editable={false}
-                    />
-                  )}
-                  {userPersonalDetails && (
-                    <PersonalDetailsGrid
-                      id="work"
-                      lable="Work Location"
-                      value={userPersonalDetails.workLocation}
-                      editable={false}
-                    />
-                  )}
-                  {userPersonalDetails && (
-                    <PersonalDetailsGrid
-                      id="address"
-                      lable="Residantal Address"
-                      value={userPersonalDetails.address}
-                      editable={true}
-                    />
-                  )}
-                </CardContent>
-                <CardActions sx={{width:'92%',justifyContent:"flex-end"}}>
-                  <UpdateButton />
-                </CardActions>
-              </Card>
-            
-          </Box> */}
+        <Typography variant="h4">My Reservation</Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: { xs: "3% 0 20% 0" },
+          }}
+        >
+          <Card sx={{ width: { xs: "100%", sm: "90%" } }}>
+            <CardContent sx={{ display: "flex", flexDirection: "column" }}>
+              <Tabs
+                onChange={handleChange}
+                aria-label="lab API tabs example"
+                sx={{textTransform:'capitalize'}}
+              >
+                <Tab label="Ongoing Reservation" value="0" />
+                <Tab label="Past Reservation" value="1" />
+              </Tabs>
+              {tabComponent[value]}
+            </CardContent>
+          </Card>
+        </Box>
       </Box>
     </ThemeProvider>
   );
