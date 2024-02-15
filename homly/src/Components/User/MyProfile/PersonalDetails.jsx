2473cb050c7418ffd027a1b2d141a34a149d4b75
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-02-16 00:16:14 +0530

    Update AuthContext and PersonalDetailsGrid components

--- homly/src/Components/User/MyProfile/PersonalDetails.jsx
+++ homly/src/Components/User/MyProfile/PersonalDetails.jsx
@@ -7,16 +7,18 @@ import {
   CardContent,
   CardActions,
   Button,
+  Stack,
+  Avatar,
 } from "@mui/material";
 
 import PersonalDetailsGrid from "../PersonalDetailsGrid/PersonalDetailsGrid";
 
 import theme from "../../../HomlyTheme";
+import ProfilePicUploadPopup from "../ProfilePicUploadPopup";
 
 const PersonalDetails = () => {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   const phoneRegex = /^[0-9]{10}$/;
-
 
   const [data, setData] = useState({
     serviceNo: "214002",
@@ -26,9 +28,15 @@ const PersonalDetails = () => {
     address: "No 1, Colombo",
     contactNo: "0123456798",
     email: "apb@gmail.com",
+    image: "",
   });
 
   const [isEnable, setIsEnable] = useState(false);
+
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
   const checkEmail = (email) => {
     return email.length > 0 && !emailRegex.test(email);
@@ -64,66 +72,113 @@ const PersonalDetails = () => {
           }}
         >
           <Card sx={{ width: { xs: "100%", sm: "90%" } }}>
-            <CardContent sx={{ display: "flex", flexDirection: "column" }}>
-              <PersonalDetailsGrid
-                id="serviceNo"
-                lable="Service Number"
-                value={data.serviceNo}
-                editable={false}
-              />
-              <PersonalDetailsGrid
-                id="name"
-                lable="Name"
-                value={data.name}
-                editable={false}
-              />
-              <PersonalDetailsGrid
-                id="nic"
-                lable="NIC Number"
-                value={data.nic}
-                editable={false}
-              />
-              <PersonalDetailsGrid
-                id="work"
-                lable="Work Location"
-                value={data.work}
-                editable={false}
-              />
-              <PersonalDetailsGrid
-                id="address"
-                lable="Residantal Address"
-                value={data.address}
-                editable={false}
-              />
-              <PersonalDetailsGrid
-                id="contactNo"
-                lable="Contact Number"
-                value={data.contactNo}
-                editable={isEnable}
-                setData={(value) => {
-                  setData({ ...data, contactNo: value });
-                }}
-                error={checkContactNo(data.contactNo)}
-                helperText={checkContactNo(data.contactNo) ? "Invalid Contact Number" : ""}
-              />
-              <PersonalDetailsGrid
-                id="email"
-                lable="Email"
-                value={data.email}
-                editable={isEnable}
-                setData={(value) => {
-                  setData({ ...data, email: value });
-                }}
-                error={checkEmail(data.email)}
-                helperText={checkEmail(data.email) ? "Invalid Email" : ""}
-              />
+            <CardContent>
+              <Stack direction="row" sx={{ flexWrap:'wrap' }}>
+                <Box sx={{ width:{xs:'100%',sm:'70%'} }}>
+                  <PersonalDetailsGrid
+                    id="serviceNo"
+                    lable="Service Number"
+                    value={data.serviceNo}
+                    editable={false}
+                  />
+                  <PersonalDetailsGrid
+                    id="name"
+                    lable="Name"
+                    value={data.name}
+                    editable={false}
+                  />
+                  <PersonalDetailsGrid
+                    id="nic"
+                    lable="NIC Number"
+                    value={data.nic}
+                    editable={false}
+                  />
+                  <PersonalDetailsGrid
+                    id="work"
+                    lable="Work Location"
+                    value={data.work}
+                    editable={false}
+                  />
+                  <PersonalDetailsGrid
+                    id="address"
+                    lable="Residantal Address"
+                    value={data.address}
+                    editable={false}
+                  />
+                  <PersonalDetailsGrid
+                    id="contactNo"
+                    lable="Contact Number"
+                    value={data.contactNo}
+                    editable={isEnable}
+                    setData={(value) => {
+                      setData({ ...data, contactNo: value });
+                    }}
+                    error={checkContactNo(data.contactNo)}
+                    helperText={
+                      checkContactNo(data.contactNo)
+                        ? "Invalid Contact Number"
+                        : ""
+                    }
+                  />
+                  <PersonalDetailsGrid
+                    id="email"
+                    lable="Email"
+                    value={data.email}
+                    editable={isEnable}
+                    setData={(value) => {
+                      setData({ ...data, email: value });
+                    }}
+                    error={checkEmail(data.email)}
+                    helperText={checkEmail(data.email) ? "Invalid Email" : ""}
+                  />
+                </Box>
+                <Box sx={{ height:'210px',width:{xs:'100%',sm:'20%'} }}>
+                  {/* <AvatarImage /> */}
+                  <ProfilePicUploadPopup
+                    open={open}
+                    setOpen={setOpen}
+                    setImage={(image) => setData({ ...data, image: image })}
+                  />
+                  <Stack
+                    direction="column"
+                    sx={{
+                      margin: "2% 0",
+                      height: { xs: 80, sm: 100 },
+                      width: "100%",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Avatar
+                      alt="Remy Sharp"
+                      src={data.image}
+                      sx={{
+                        width: 150,
+                        height: 150,
+                      }}
+                    />
+                    <Box
+                      sx={{
+                        height: "100%",
+                        display: "flex",
+                        alignItems: "center",
+                        marginTop: "15px",
+                        marginLeft:  "8px" ,
+                      }}
+                    >
+                      <Button variant="outlined" onClick={handleClickOpen} disabled={!isEnable}>
+                        Edit Profile Picture
+                      </Button>
+                    </Box>
+                  </Stack>
+                </Box>
+              </Stack>
             </CardContent>
-            <CardActions sx={{justifyContent:'flex-end'}}>
+            <CardActions sx={{ justifyContent: "flex-end" }}>
               <Button
                 variant="outlined"
                 size="small"
                 onClick={handleCancel}
-                sx={{ width: "70px"}}
+                sx={{ width: "70px" }}
               >
                 Cancel
               </Button>
@@ -131,7 +186,11 @@ const PersonalDetails = () => {
                 <Button
                   variant="contained"
                   size="small"
-                  sx={{ backgroundColor: "primary.main", marginLeft: "2%",width:'70px' }}
+                  sx={{
+                    backgroundColor: "primary.main",
+                    marginLeft: "2%",
+                    width: "70px",
+                  }}
                   onClick={handleUpdate}
                 >
                   Update
@@ -140,7 +199,11 @@ const PersonalDetails = () => {
                 <Button
                   variant="contained"
                   size="small"
-                  sx={{ backgroundColor: "primary.main", marginLeft: "2%",width:'70px' }}
+                  sx={{
+                    backgroundColor: "primary.main",
+                    marginLeft: "2%",
+                    width: "70px",
+                  }}
                   onClick={handleEdit}
                 >
                   Edit
