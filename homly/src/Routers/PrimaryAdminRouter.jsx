1483aa0c3c48be01c3166ab21f8a00641cfa9113
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-02-15 10:00:50 +0530

    create primary admin router

---
+++ homly/src/Routers/PrimaryAdminRouter.jsx
@@ -0,0 +1,72 @@
+import React, { lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const PrimaryDashboard = lazy(() =>
+  import("../Pages/PrimaryAdmin/PrimaryDashboard")
+);
+const PrimaryReservations = lazy(() =>
+  import("../Pages/PrimaryAdmin/PrimaryReservations")
+);
+const PrimaryHolidayHomes = lazy(() =>
+  import("../Pages/PrimaryAdmin/PrimaryHolidayHomes")
+);
+const PrimaryReport = lazy(() => import("../Pages/PrimaryAdmin/PrimaryReport"));
+const PrimaryManageBlacklistedUsers = lazy(() =>
+  import("../Pages/PrimaryAdmin/PrimaryManageBlacklistedUsers")
+);
+const PrimaryBlacklistHistory = lazy(() =>
+  import("../Pages/PrimaryAdmin/PrimaryBlacklistHistory")
+);
+const PrimaryComplaints = lazy(() =>
+  import("../Pages/PrimaryAdmin/PrimaryComplaints")
+);
+const PrimaryAddAdmin = lazy(() =>
+  import("../Pages/PrimaryAdmin/PrimaryAddAdmin")
+);
+const PrimaryViewAdmin = lazy(() =>
+  import("../Pages/PrimaryAdmin/PrimaryViewAdmins")
+);
+const PrimaryAuthorizations = lazy(() =>
+  import("../Pages/PrimaryAdmin/PrimaryAuthorizations")
+);
+
+const PrimaryAdminRouter = () => (
+  <Routes>
+    <Route
+      path="/primaryadmin/dashboard"
+      element={<PrimaryDashboard />}
+    ></Route>
+    <Route
+      path="/primaryadmin/reservations"
+      element={<PrimaryReservations />}
+    ></Route>
+    <Route
+      path="/primaryadmin/holidayhomes"
+      element={<PrimaryHolidayHomes />}
+    ></Route>
+    <Route path="/primaryadmin/report" element={<PrimaryReport />}></Route>
+    <Route
+      path="/primaryadmin/blacklistedusers/manage"
+      element={<PrimaryManageBlacklistedUsers />}
+    ></Route>
+    <Route
+      path="/primaryadmin/blacklistedusers/history"
+      element={<PrimaryBlacklistHistory />}
+    ></Route>
+    <Route
+      path="/primaryadmin/blacklistedusers/complaints"
+      element={<PrimaryComplaints />}
+    ></Route>
+    <Route path="/primaryadmin/addadmin" element={<PrimaryAddAdmin />}></Route>
+    <Route
+      path="/primaryadmin/viewadmin"
+      element={<PrimaryViewAdmin />}
+    ></Route>
+    <Route
+      path="/primaryadmin/authorizations"
+      element={<PrimaryAuthorizations />}
+    ></Route>
+  </Routes>
+);
+
+export default PrimaryAdminRouter;
