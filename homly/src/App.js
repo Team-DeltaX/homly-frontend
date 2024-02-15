5373e3d609fc127d40c7e9c108a4d0dfade32354
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-02-15 09:47:32 +0530

    update app.js

--- homly/src/App.js
+++ homly/src/App.js
@@ -1,13 +1,15 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Loader from "./Components/Loader/Loader";
+
+import UserRouter from "./Routers/UserRouter";
+
 import AuthContextProvider from "./Contexts/AuthContext";
+
 import "../src/Styles/styles.css";
+
+
 import ManageHomes from "../src/Pages/locationAdmin/ManageHomes";
 import HolidayHomeEdit from "./Pages/locationAdmin/HolidayHomeEdit";
-
-
-
 
 // user pages import start
 
@@ -46,114 +48,64 @@ import UserLogin from "../src/Pages/User/UserLogin";
 // import UserSecurity from "../src/Pages/User/UserSecurity";
 // import PaymentDetails from "../src/Pages/User/PaymentDetails";
 // import MyReservation from "../src/Pages/User/MyReservation";
-import HolidayHomeDetails from '../src/Pages/User/HolidayHomeDetails';
+import HolidayHomeDetails from "../src/Pages/User/HolidayHomeDetails";
 import EmailVerified from "./Pages/User/EmailVerified";
 
-const PrimaryDashboard = lazy(() => import("./Pages/PrimaryAdmin/PrimaryDashboard"));
-const PrimaryReservations = lazy(() => import("./Pages/PrimaryAdmin/PrimaryReservations"));
-const PrimaryHolidayHomes = lazy(() => import("./Pages/PrimaryAdmin/PrimaryHolidayHomes"));
+const PrimaryDashboard = lazy(() =>
+  import("./Pages/PrimaryAdmin/PrimaryDashboard")
+);
+const PrimaryReservations = lazy(() =>
+  import("./Pages/PrimaryAdmin/PrimaryReservations")
+);
+const PrimaryHolidayHomes = lazy(() =>
+  import("./Pages/PrimaryAdmin/PrimaryHolidayHomes")
+);
 const PrimaryReport = lazy(() => import("./Pages/PrimaryAdmin/PrimaryReport"));
-const PrimaryManageBlacklistedUsers = lazy(() => import("./Pages/PrimaryAdmin/PrimaryManageBlacklistedUsers"));
-const PrimaryBlacklistHistory = lazy(() => import("./Pages/PrimaryAdmin/PrimaryBlacklistHistory"));
-const PrimaryComplaints = lazy(() => import("./Pages/PrimaryAdmin/PrimaryComplaints"));
-const PrimaryAddAdmin = lazy(() => import("./Pages/PrimaryAdmin/PrimaryAddAdmin"));
-const PrimaryViewAdmin = lazy(() => import("./Pages/PrimaryAdmin/PrimaryViewAdmins"));
-const PrimaryAuthorizations = lazy(() => import("./Pages/PrimaryAdmin/PrimaryAuthorizations"));
-
+const PrimaryManageBlacklistedUsers = lazy(() =>
+  import("./Pages/PrimaryAdmin/PrimaryManageBlacklistedUsers")
+);
+const PrimaryBlacklistHistory = lazy(() =>
+  import("./Pages/PrimaryAdmin/PrimaryBlacklistHistory")
+);
+const PrimaryComplaints = lazy(() =>
+  import("./Pages/PrimaryAdmin/PrimaryComplaints")
+);
+const PrimaryAddAdmin = lazy(() =>
+  import("./Pages/PrimaryAdmin/PrimaryAddAdmin")
+);
+const PrimaryViewAdmin = lazy(() =>
+  import("./Pages/PrimaryAdmin/PrimaryViewAdmins")
+);
+const PrimaryAuthorizations = lazy(() =>
+  import("./Pages/PrimaryAdmin/PrimaryAuthorizations")
+);
 
 // user pages import end
 // location admin import start
 
 const LocationDashboard = lazy(() =>
   import("../src/Pages/locationAdmin/Dashboard")
-)
+);
 const HolidayHomesDetails = lazy(() =>
   import("../src/Pages/locationAdmin/HolidayHomeDetails")
-)
+);
 
 const CreateHolidayHome = lazy(() =>
   import("../src/Pages/locationAdmin/CreateHolidayHome")
-)
-const FeedBack = lazy(() =>
-  import("../src/Pages/locationAdmin/Feedback")
-)
+);
+const FeedBack = lazy(() => import("../src/Pages/locationAdmin/Feedback"));
 const Reservations = lazy(() =>
   import("../src/Pages/locationAdmin/Reservations")
-)
+);
 
-const Report = lazy(() =>
-  import("../src/Pages/locationAdmin/Report")
-)
+const Report = lazy(() => import("../src/Pages/locationAdmin/Report"));
 
-// const HolidayHomeEdit = lazy(()=>
-//   import("../src/Pages/locationAdmin/HolidayHomeEdit")
-// )
-
-// const LocationManageHomes =lazy(()=>{
-//   import("../src/Pages/locationAdmin/ManageHomes")
-// }) 
-
-// location admin import end
 
 const App = () => (
   <AuthContextProvider>
     <Router>
-      <Suspense fallback={<div>loading</div>}>
-        <Routes>
-          <Route path="/Home" element={<Home />} />
-          <Route path="/HolidayHomes" element={<HolidayHome />} />
-          <Route path="/HolidayHomeDetails" element={<HolidayHomeDetails />} />
-          <Route path="/MyProfile" element={<Profile />} />
-          {/* <Route path="/ContactUs" element={<ContactUs />} /> */}
-          {/* <Route path="/paymentGateway" element={<PaymentGateway />} /> */}
-          <Route path="/Registration" element={<UserRegistration />} />
-          <Route path="/" element={<UserLogin />} />
-          <Route path="/Registration/Success" element={<EmailVerified/>}/>
-          {/* <Route path="/MyProfile" element={<PersonalDetails />} /> */}
-          {/* <Route
-            path="/MyProfile/PersonalDetails"
-            element={<PersonalDetails />}
-          /> */}
-          {/* <Route path="/MyProfile/Security" element={<UserSecurity />} />
-          <Route
-            path="/MyProfile/PaymentDetails"
-            element={<PaymentDetails />}
-          /> */}
-          {/* <Route path="/MyProfile/MyReservation" element={<MyReservation />} /> */}
-
-          <Route path="/locationadmin/dashboard" element={<LocationDashboard />} />
-          <Route path="/locationadmin/manage" element={<ManageHomes />} />
-          <Route path="/locationadmin/details" element={<HolidayHomesDetails />} />
-          <Route path="/locationadmin/feedback" element={<FeedBack />} />
-          <Route path="/locationadmin/reservations" element={<Reservations />} />
-          <Route path="/locationadmin/report" element={<Report />} />
-          <Route path="/locationadmin/holidayhomes/createholidayhome" element={<CreateHolidayHome />} />
-          <Route path="/locationadmin/holidayhomes/editholidayhome" element={<HolidayHomeEdit />} />
-
-
-
-
-
-
-
-          <Route path='/primaryadmin/dashboard' element={<PrimaryDashboard />}></Route>
-          <Route path='/primaryadmin/reservations' element={<PrimaryReservations />}></Route>
-          <Route path='/primaryadmin/holidayhomes' element={<PrimaryHolidayHomes />}></Route>
-          <Route path='/primaryadmin/report' element={<PrimaryReport />}></Route>
-          <Route path='/primaryadmin/blacklistedusers/manage' element={<PrimaryManageBlacklistedUsers />}></Route>
-          <Route path='/primaryadmin/blacklistedusers/history' element={<PrimaryBlacklistHistory />}></Route>
-          <Route path='/primaryadmin/blacklistedusers/complaints' element={<PrimaryComplaints />}></Route>
-          <Route path='/primaryadmin/addadmin' element={<PrimaryAddAdmin />}></Route>
-          <Route path='/primaryadmin/viewadmin' element={<PrimaryViewAdmin />}></Route>
-          <Route path='/primaryadmin/authorizations' element={<PrimaryAuthorizations />}></Route>
-
-
-
-
-
-
-
-        </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <UserRouter />
       </Suspense>
     </Router>
   </AuthContextProvider>
