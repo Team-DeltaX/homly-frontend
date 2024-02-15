2e6ceb582a0afcce24ce2d2165cd15f7d90917e9
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-02-15 09:47:05 +0530

    Add UserRouter component for user-specific routes

---
+++ homly/src/Routers/UserRouter.jsx
@@ -0,0 +1,26 @@
+import React, {  lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const UserRegistration = lazy(() => import("../Pages/User/UserRegistration"));
+const UserLogin = lazy(() => import("../Pages/User/UserLogin"));
+const Home = lazy(() => import("../Pages/User/Home"));
+const HolidayHome = lazy(() => import("../Pages/User/HolidayHome"));
+const Profile = lazy(() => import("../Pages/User/Profile"));
+const HolidayHomeDetails = lazy(() =>
+  import("../Pages/User/HolidayHomeDetails")
+);
+const EmailVerified = lazy(() => import("../Pages/User/EmailVerified"));
+
+const UserRouter = () => (
+    <Routes>
+      <Route path="/" element={<UserLogin />} />
+      <Route path="/Registration" element={<UserRegistration />} />
+      <Route path="/Home" element={<Home />} />
+      <Route path="/HolidayHomes" element={<HolidayHome />} />
+      <Route path="/HolidayHomeDetails" element={<HolidayHomeDetails />} />
+      <Route path="/MyProfile" element={<Profile />} />
+      <Route path="/Registration/Success" element={<EmailVerified />} />
+    </Routes>
+);
+
+export default UserRouter;
