2c1e2121ac0da28a626fd6336977abded35524ef
Booshana Namudara <booshiwork@gmail.com>
2024-02-26 11:13:30 +0530

    update the meaningful description to HH

--- homly/src/Pages/User/HolidayHomeDetails.jsx
+++ homly/src/Pages/User/HolidayHomeDetails.jsx
@@ -121,11 +121,7 @@ export default function HolidayHomeDetails() {
                   </Stack>
 
                   <Typography variant="subtitle1" gutterBottom>
-                    Descreption body1. Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Quos blanditiis tenetur unde suscipit,
-                    quam beatae rerum inventore consectetur, neque doloribus,
-                    cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-                    quasi quidem quibusdam.
+                    Discover the epitome of relaxation at Serene Haven - your idyllic holiday sanctuary. Nestled amidst lush greenery, our cozy accommodations offer the perfect escape. Unwind in comfort, explore nearby adventures, and create cherished memories. Book your stay now for an unforgettable retreat!
                   </Typography>
                 </Stack>
               </Grid>
