b81fe23a2c0343ce4437b9dd473c2d37a80c96ec
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-02-27 14:30:52 +0530

    add toggle button

--- homly/src/Components/User/UserInterestedPopup.jsx
+++ homly/src/Components/User/UserInterestedPopup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -6,13 +6,43 @@ import {
   DialogContentText,
   DialogActions,
   Button,
+  ThemeProvider,
+  Box,
+  ToggleButtonGroup,
+  ToggleButton,
 } from "@mui/material";
+import theme from "../../HomlyTheme";
+
+// change toggle button style
+const style = {
+  margin: "2px",
+  border: "1px solid #872341",
+  borderRadius: "50px",
+  padding: "5px 30px",
+  
+  
+}
 
 export default function UserInterestedPopup({ open, setOpen }) {
+  const [interests, setInterests] = useState([]);
+  const [count, setCount] = useState(0);
+
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handleFormat = (event, newInterest) => {
+    if(newInterest.length > 3){
+
+    }else{
+      setInterests(newInterest);
+      setCount(count + 1);
+    }
+    console.log(interests);
+  };
+
   return (
+    <ThemeProvider theme={theme}>
     <Dialog open={open} keepMounted onClose={handleClose}>
       <DialogTitle>Choose Your Interest</DialogTitle>
       <DialogContent>
@@ -20,11 +50,34 @@ export default function UserInterestedPopup({ open, setOpen }) {
           Hello there! Let's customize your experience. Pick your top 3
           interests in order
         </DialogContentText>
+        <Box sx={{margin:"7px 0"}}>
+          <ToggleButtonGroup
+            value={interests}
+            onChange={handleFormat}
+            aria-label="interests facilities"
+            sx={{
+                flexWrap: "wrap",
+              
+              ".css-q9gk48-MuiButtonBase-root-MuiToggleButton-root.Mui-selected, .css-q9gk48-MuiButtonBase-root-MuiToggleButton-root.Mui-selected:hover": {
+                backgroundColor: "#f8abc3",
+              },
+            }}
+          >
+            
+            <ToggleButton value="food" aria-label="food" style={style}>Food</ToggleButton>
+            <ToggleButton value="location" aria-label="location" style={style}>Location</ToggleButton>
+            <ToggleButton value="wifi" aria-label="wifi" style={style}>wifi</ToggleButton>
+            <ToggleButton value="staff" aria-label="staff" style={style}>Staff</ToggleButton>
+            <ToggleButton value="money" aria-label="money" style={style}>Value For money</ToggleButton>
+            <ToggleButton value="bold6" aria-label="food6" style={style}>Food</ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>close</Button>
         <Button onClick={handleClose}>Agree</Button>
       </DialogActions>
     </Dialog>
+    </ThemeProvider>
   );
 }
