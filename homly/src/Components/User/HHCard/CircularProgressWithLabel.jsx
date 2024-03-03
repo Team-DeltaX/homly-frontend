8103992b80eb0710ff05ecbd2613df5724ae0ac4
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-03-03 23:53:17 +0530

    add new user interest popup to update interests

--- homly/src/Components/User/HHCard/CircularProgressWithLabel.jsx
+++ homly/src/Components/User/HHCard/CircularProgressWithLabel.jsx
@@ -32,6 +32,7 @@ const CircularProgressWithLabel = ({ label, value }) => {
 
   useEffect(() => {
     setFacility(changeFacilityName(label));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
