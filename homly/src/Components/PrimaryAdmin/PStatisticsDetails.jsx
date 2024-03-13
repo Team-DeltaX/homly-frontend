da87d2982a52d4730e8af7326bbefc6f15c784ad
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2024-03-13 12:42:20 +0530

    Pastatistics details component in the Pdashboardcon fully connected with backend apis

--- homly/src/Components/PrimaryAdmin/PStatisticsDetails.jsx
+++ homly/src/Components/PrimaryAdmin/PStatisticsDetails.jsx
@@ -1,19 +1,16 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
-import { Grid } from '@mui/material';
-import CircularProgress from '@mui/material/CircularProgress';
+import { Grid } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+import axios from "axios";
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
-
-  const [unitcount,setUnitcount]=React.useState("0")
-  const [unitrevenue,setUnitrevenue]=React.useState("0")
-  
 
   return (
     <div
@@ -41,80 +38,215 @@ CustomTabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
 export default function StatisticsDetails() {
   const [value, setValue] = React.useState(0);
 
+  const [roomcount, setRoomcount] = React.useState(0);
+  const [hallcount, setHallcount] = React.useState(0);
+  const [roomrevenue, setRoomrevenue] = React.useState(0);
+  const [hallrevenue, setHallrevenue] = React.useState(0);
+
+
+  const getRoomCount = () => {
+    axios.get('http://localhost:3002/admin/auth/room')
+    .then((res)=>{
+      setRoomcount(res.data.count)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+
+  const getHallCount = () => {
+    axios.get('http://localhost:3002/admin/auth/hall')
+    .then((res)=>{
+      setHallcount(res.data.count)
+    })
+  }
+  const getHallincome=()=>{
+    axios.get('http://localhost:3002/admin/auth/hallincome')
+    .then((res)=>{
+      setHallrevenue(res.data.hallincome)
+    
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+  const getRoomincome=()=>{
+    axios.get('http://localhost:3002/admin/auth/roomincome')
+    .then((res)=>{
+      setRoomrevenue(res.data.roomincome)
+    }).catch((err)=>{ 
+      console.log(err)
+    })
+  }
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  React.useEffect(()=>{
+    getRoomCount()
+    getHallCount()
+    getHallincome()
+    getRoomincome()
+
+  },[])
 
   return (
-    
-    <Box sx={{ width: '100%',paddingRight:'32px' }}>
-      <Box sx={{ border: 1,borderTopLeftRadius:20,borderTopRightRadius:20, borderColor: 'divider',width:"100%"}}>
-        <Tabs value={value} onChange={handleChange} variant='fullWidth' aria-label="basic tabs example" sx={{}}>
-          <Tab sx={{fontWeight:"bold"}} label="Rooms" {...a11yProps(0)} />
-          <Tab sx={{fontWeight:"bold"}} label="Halls" {...a11yProps(1)} />
+    <Box sx={{ width: "100%", paddingRight: "32px" }}>
+      <Box
+        sx={{
+          border: 1,
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          borderColor: "divider",
+          width: "100%",
+        }}
+      >
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="fullWidth"
+          aria-label="basic tabs example"
+          sx={{}}
+        >
+          <Tab sx={{ fontWeight: "bold" }} label="Rooms" {...a11yProps(0)} />
+          <Tab sx={{ fontWeight: "bold" }} label="Halls" {...a11yProps(1)} />
         </Tabs>
       </Box>
-      <Box sx={{backgroundColor:"background.lightgrey",width:"100%"}}>
+      <Box sx={{ backgroundColor: "background.lightgrey", width: "100%" }}>
         <CustomTabPanel value={value} index={0}>
-            <Grid container spacing={2}>
-                <Grid item md={6} >
-                    <Box sx={{backgroundColor:'white',borderRadius:'20px',display:'flex',flexWrap:'wrap',alignItems:'center',justifyContent:'space-between',padding:"30px 20px"}}>
-                        <Box>
-                            <Typography variant='h5' fontWeight={'bold'}>72</Typography>
-                            <Typography variant='p' sx={{color:"grey6",fontSize:'0.8rem'}}>Total number of Rooms</Typography>
-                        </Box>
-                        <Box>
-                            <CircularProgress  variant="determinate" value={25} />
-                        </Box>
-                    </Box>
-                </Grid>
-                <Grid item md={6}>
-                    <Box sx={{backgroundColor:'white',borderRadius:'20px',display:'flex',flexWrap:'wrap',alignItems:'center',justifyContent:'space-between',padding:"30px 20px"}}>
-                        <Box>
-                            <Typography variant='h5' fontWeight={'bold'}>120,000LKR</Typography>
-                            <Typography variant='p' sx={{color:"grey6",fontSize:'0.8rem'}}>Total Revenue from Rooms</Typography>
-                        </Box>
-                        <Box>
-                            <CircularProgress sx={{color:"#11BA40"}} variant="determinate" value={75} />
-                        </Box>
-                    </Box>
-                </Grid>
-
+          <Grid container spacing={2}>
+            <Grid item md={6}>
+              <Box
+                sx={{
+                  backgroundColor: "white",
+                  borderRadius: "20px",
+                  display: "flex",
+                  flexWrap: "wrap",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  padding: "30px 20px",
+                }}
+              >
+                <Box>
+                  <Typography variant="h5" fontWeight={"bold"}>
+                    {roomcount}
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    sx={{ color: "grey6", fontSize: "0.8rem" }}
+                  >
+                    Total number of Rooms
+                  </Typography>
+                </Box>
+                <Box>
+                  <CircularProgress variant="determinate" value={25} />
+                </Box>
+              </Box>
             </Grid>
+            <Grid item md={6}>
+              <Box
+                sx={{
+                  backgroundColor: "white",
+                  borderRadius: "20px",
+                  display: "flex",
+                  flexWrap: "wrap",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  padding: "30px 20px",
+                }}
+              >
+                <Box>
+                  <Typography variant="h5" fontWeight={"bold"}>
+                    {roomrevenue}
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    sx={{ color: "grey6", fontSize: "0.8rem" }}
+                  >
+                    Total Revenue from Rooms
+                  </Typography>
+                </Box>
+                <Box>
+                  <CircularProgress
+                    sx={{ color: "#11BA40" }}
+                    variant="determinate"
+                    value={75}
+                  />
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
         </CustomTabPanel>
         <CustomTabPanel value={value} index={1}>
-        <Grid container spacing={2}>
-                <Grid item md={6} >
-                    <Box sx={{backgroundColor:'white',borderRadius:'20px',display:'flex',flexWrap:'wrap',alignItems:'center',justifyContent:'space-between',padding:"30px 20px"}}>
-                        <Box>
-                            <Typography variant='h5' fontWeight={'bold'}>72</Typography>
-                            <Typography variant='p' sx={{color:"grey6",fontSize:'0.8rem'}}>Total number of Halls</Typography>
-                        </Box>
-                        <Box>
-                            <CircularProgress  variant="determinate" value={100} />
-                        </Box>
-                    </Box>
-                </Grid>
-                <Grid item md={6}>
-                    <Box sx={{backgroundColor:'white',borderRadius:'20px',display:'flex',flexWrap:'wrap',alignItems:'center',justifyContent:'space-between',padding:"30px 20px"}}>
-                        <Box>
-                            <Typography variant='h5' fontWeight={'bold'}>120,000LKR</Typography>
-                            <Typography variant='p' sx={{color:"grey6",fontSize:'0.8rem'}}>Total Revenue from Halls</Typography>
-                        </Box>
-                        <Box>
-                            <CircularProgress sx={{color:"#11BA40",zIndex:'100'}} variant="determinate" value={50} />
-                        </Box>
-                    </Box>
-                </Grid>
-
+          <Grid container spacing={2}>
+            <Grid item md={6}>
+              <Box
+                sx={{
+                  backgroundColor: "white",
+                  borderRadius: "20px",
+                  display: "flex",
+                  flexWrap: "wrap",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  padding: "30px 20px",
+                }}
+              >
+                <Box>
+                  <Typography variant="h5" fontWeight={"bold"}>
+                    {hallcount}
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    sx={{ color: "grey6", fontSize: "0.8rem" }}
+                  >
+                    Total number of Halls
+                  </Typography>
+                </Box>
+                <Box>
+                  <CircularProgress variant="determinate" value={100} />
+                </Box>
+              </Box>
             </Grid>
+            <Grid item md={6}>
+              <Box
+                sx={{
+                  backgroundColor: "white",
+                  borderRadius: "20px",
+                  display: "flex",
+                  flexWrap: "wrap",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  padding: "30px 20px",
+                }}
+              >
+                <Box>
+                  <Typography variant="h5" fontWeight={"bold"}>
+                    {hallrevenue}
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    sx={{ color: "grey6", fontSize: "0.8rem" }}
+                  >
+                    Total Revenue from Halls
+                  </Typography>
+                </Box>
+                <Box>
+                  <CircularProgress
+                    sx={{ color: "#11BA40", zIndex: "100" }}
+                    variant="determinate"
+                    value={50}
+                  />
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
         </CustomTabPanel>
       </Box>
     </Box>
