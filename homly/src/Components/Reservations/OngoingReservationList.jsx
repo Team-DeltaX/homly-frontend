06ed2f000c33bdafbc2981a78844c8360eb7a6de
Booshana Namudara <booshiwork@gmail.com>
2024-04-29 18:15:28 +0530

    updated code

--- homly/src/Components/Reservations/OngoingReservationList.jsx
+++ homly/src/Components/Reservations/OngoingReservationList.jsx
@@ -12,11 +12,9 @@ import AxiosClient from "../../services/AxiosClient";
 const OngoingReservationList = (props) => {
   const [reservations, setReservations] = useState([])
   const fetchreservations = () => {
-    // axios
-    //   .get("http://localhost:8080/user/reservation/ongoing")
     AxiosClient.get("/user/reservation/ongoing")
       .then((res) => {
-        console.log(res.data);
+        console.log("fbnh fjnygfvfrvegbh",res.data);
          //reverse array to keep new ones first 
         setReservations(res.data.reverse());
       })
@@ -30,22 +28,9 @@ const OngoingReservationList = (props) => {
   }, []);
   return (
     <>
-        {/* <Box className="home">
-      {reservations.filter((reservations) => {
-                    return props.search.toLowerCase() === ""
-                      ? reservations
-                      : reservations.empName.toLowerCase().startsWith(
-                            props.search.toLocaleLowerCase()
-                        );
-                  })
-                  .map(reservation => (
-         (<OngoingReservationCard  reservation={reservation.Reservations[0]} name={reservation.empName}/>)    
-      ))}
-    </Box> */}
     <Box className="home">
       {reservations.map(reservation => (
-         <OngoingReservationCard  reservation={reservation.reservation} reservedRoom={reservation.reservedrooms}/>
-        // console.log("sadasd",reservation)
+         <OngoingReservationCard holidayHome={reservation.holidayHome[0]} reservation={reservation.reservation} reservedRoom={reservation.reservedrooms} reservedHall={reservation.reservedhalls}/>
       ))}
     </Box>
     </>
