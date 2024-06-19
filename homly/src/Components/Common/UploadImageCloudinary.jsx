d0d4873aa171c8e1f9e1ab585f8bb19f1c12c58b
Booshana Namudara <booshiwork@gmail.com>
2024-06-19 08:34:13 +0530

    update

--- homly/src/Components/Common/UploadImageCloudinary.jsx
+++ homly/src/Components/Common/UploadImageCloudinary.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import { Stack, ThemeProvider, Button, Typography } from "@mui/material";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import theme from "../../HomlyTheme";
 
 const UploadImageCloudinary = ({
@@ -58,6 +59,7 @@ const UploadImageCloudinary = ({
         <Button
           variant={buttonVariant ? buttonVariant : "outlined"}
           size={buttonSize ? buttonSize : "small"}
+          startIcon={<CloudUploadIcon />}
           disabled={isDisabled ? isDisabled : false}
           onClick={() => widgetRef.current.open()}
         >
