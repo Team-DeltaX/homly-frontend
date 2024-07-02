8a977130ad7cdad6be4e2084514d7c14ff80ee80
Booshana Namudara <booshiwork@gmail.com>
2024-07-02 10:45:32 +0530

    update

--- homly/src/services/AxiosClient.js
+++ homly/src/services/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosClient = axios.create({
-  baseURL: "http://192.168.221.70:8080/",
+  baseURL: "http://localhost:8080/",
   headers: {
     "Content-Type": "application/json",
   },
